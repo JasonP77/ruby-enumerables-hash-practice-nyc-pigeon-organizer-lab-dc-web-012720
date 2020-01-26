00838f8b865ca4fca505a6c65b0d3177f7f660c3
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 06:32:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |characters, value|
-    value.each do |stats, all_names|
-      all_names.each do |name|
+    value.each do |in_characters, names|
+      names.each do |name|
         if pigeon_list[name] == nil
           pigeon_list[name] = {}
         end
         if pigeon_list[name][characters] == nil
           pigeon_list[name][characters] = []
         end
-        pigeon_list[name][characters].push(stats.to_s)
+        pigeon_list[name][characters].push(in_characters.to_s)
       end
     end 
   end
